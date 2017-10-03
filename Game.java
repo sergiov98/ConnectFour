5b1d98c62fc77cbbc98724ab38ef3adeffba2499
Alex Lo <Alex@alex-los-air.dynamic.ucsd.edu>
2017-10-02 18:24:16 -0700

    Finished text version of game

--- Game.java
+++ Game.java
@@ -1,9 +1,44 @@
+import java.util.*;
+
 /*
  * The controller
  * Translates user actions into operations on the model
  */
 public class Game{
+  
   public static void main(String[] args) {
-    Board connect_four_board = new Board();
+    
+    //TODO UNCOMMENT
+    //Board connect_four_board = new Board();
+
+    // TEMP while gui doesnt work
+    Model m = new Model();
+    Scanner reader = new Scanner(System.in);
+    int n;
+
+    while(m.gameOver() == 0) {
+      m.print();
+      System.out.println("Player " + m.getTurn() + "'s turn: ");
+      System.out.println("Enter a column to place a piece in.");
+      n = reader.nextInt() - 1;
+      if(n < 0 || n > Model.ROWS) {
+        System.out.println("Invalid column! Try again.");
+        continue;
+      }
+      if(!m.dropPiece(n)) {
+        System.out.println("Column is full! Try again.");
+        continue;
+      }
+      m.switchTurn();
+    }
+
+    m.print();
+    System.out.println("Game over!");
+    if(m.gameOver() == 3) {
+      System.out.println("It's a draw!");
+    } else {
+      System.out.println("Player " + m.gameOver() + " wins!");
+    }
+    // TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP
   }
 }
--- Model.java
+++ Model.java
@@ -4,22 +4,30 @@
  */
 public class Model {
   
+  final static int WIN = 4;
   final static int ROWS = 6;
   final static int COLS = 7;
-  private Piece[][] grid;
+  private int[][] grid;
   private int playerTurn;
+  private int recentDropR;
+  private int recentDropC;
+  private int tempR;
+  private int tempC;
+  private int inARow;
 
   /**
    * Constructor
    */
   public Model() {
     playerTurn = 1;
-    grid = new Piece[ROWS][COLS];
+    recentDropR = 0;
+    recentDropC = 0;
+    grid = new int[ROWS][COLS];
 
     // row numbers from bottom to top, columns from left to right
     for(int r = 0; r < ROWS; r++) {
       for(int c = 0; c < COLS; c++) {
-        grid[r][c] = new Piece();
+        grid[r][c] = 0;
       }
     }
   }
@@ -32,8 +40,10 @@ public class Model {
   public boolean dropPiece(int col) {
     // check column for empty spaces from the bottom up
     for(int r = 0; r < ROWS; r++) {
-      if(grid[r][col].getColor() == 0) {
-        grid[r][col].setColor(playerTurn);
+      if(grid[r][col] == 0) {
+        grid[r][col] = playerTurn;
+        recentDropR = r;
+        recentDropC = col;
         return true;
       }
     }
@@ -48,12 +58,128 @@ public class Model {
    *         0 for not game over, 1 for p1, 2 for p2, 3 for draw
    */
   public int gameOver() {
-    // TODO check 4 in a row
+    // check for 4 in a row
+
+    // horizontally
+    inARow = 0;
+    for(int c = 0; c < COLS; c++) {
+      if(grid[recentDropR][c] == 1) {
+        if(inARow <= 0) {
+          inARow = 1;
+        } else {
+          inARow++;
+        }
+      } else if(grid[recentDropR][c] == 2) {
+        if(inARow >= 0) {
+          inARow = -1;
+        } else {
+          inARow--;
+        }
+      } else {
+        inARow = 0;
+      }
+      if(inARow == WIN) {
+        return 1;
+      } else if(inARow == -WIN) {
+        return 2;
+      }
+    }
+
+    // vertically 
+    inARow = 0;
+    for(int r = 0; r < ROWS; r++) {
+      if(grid[r][recentDropC] == 1) {
+        if(inARow <= 0) {
+          inARow = 1;
+        } else {
+          inARow++;
+        }
+      } else if(grid[r][recentDropC] == 2) {
+        if(inARow >= 0) {
+          inARow = -1;
+        } else {
+          inARow--;
+        }
+      } else {
+        inARow = 0;
+      }
+      if(inARow == WIN) {
+        return 1;
+      } else if(inARow == -WIN) {
+        return 2;
+      }
+    }
+
+    // diagonally (bottom-left to top-right)
+    inARow = 0; 
+    tempR = recentDropR;
+    tempC = recentDropC;
+    while(tempR > 0 && tempC > 0) {
+      tempR--;
+      tempC--;
+    }
+    while(tempR< ROWS && tempC < COLS) {
+      if(grid[tempR][tempC] == 1) {
+        if(inARow <= 0) {
+          inARow = 1;
+        } else {
+          inARow++;
+        }
+      } else if(grid[tempR][tempC] == 2) {
+        if(inARow >= 0) {
+          inARow = -1;
+        } else {
+          inARow--;
+        }
+      } else {
+        inARow = 0;
+      }
+      if(inARow == WIN) {
+        return 1;
+      } else if(inARow == -WIN) {
+        return 2;
+      }
+      tempR++;
+      tempC++;
+    }
+
+    // diagonally (top-left to bottom-right)
+    inARow = 0; 
+    tempR = recentDropR;
+    tempC = recentDropC;
+    while(tempR < ROWS - 1 && tempC > 0) {
+      tempR++;
+      tempC--;
+    }
+    while(tempR >= 0 && tempC < COLS) {
+      if(grid[tempR][tempC] == 1) {
+        if(inARow <= 0) {
+          inARow = 1;
+        } else {
+          inARow++;
+        }
+      } else if(grid[tempR][tempC] == 2) {
+        if(inARow >= 0) {
+          inARow = -1;
+        } else {
+          inARow--;
+        }
+      } else {
+        inARow = 0;
+      }
+      if(inARow == WIN) {
+        return 1;
+      } else if(inARow == -WIN) {
+        return 2;
+      }
+      tempR--;
+      tempC++;
+    }
 
     // if any space is unfilled, game is not over yet
     for(int r = 0; r < ROWS; r++) {
       for(int c = 0; c < COLS; c++) {
-        if(grid[r][c].getColor() == 0) {
+        if(grid[r][c] == 0) {
           return 0;
         }
       }
@@ -75,4 +201,19 @@ public class Model {
   public int getTurn() {
     return playerTurn;
   }
-}
+
+  /**
+   * Prints the board in text form
+   */
+  public void print() {
+    System.out.println("Board:");
+    for(int r = ROWS - 1; r >= 0; r--) {
+      for(int c = 0; c < COLS; c++) {
+        System.out.print(grid[r][c] + " ");
+      }
+      System.out.println();
+      System.out.println();
+    }
+  }
+    
+} // End Model Class
